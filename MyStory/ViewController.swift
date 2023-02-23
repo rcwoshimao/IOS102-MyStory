4d9c7e6f82dafadc02c611a6dcd88ff546b1b4a0
rcwoshimao <mantoumiaoshen@gmail.com>
2023-02-22 22:03:48 -0800

    Initial Commit

---
+++ MyStory/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyStory
+//
+//  Created by Rebecca Chen on 2023/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
